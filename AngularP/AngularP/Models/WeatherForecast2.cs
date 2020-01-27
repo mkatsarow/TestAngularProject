ae3f0b268ad955389cb2960bdd1d1e1c865700dc
Mihail Katsarov <mihail.katsarow@gmail.com>
2020-01-28 00:18:14 +0200

    Few changes

--- AngularP/AngularP/Models/WeatherForecast2.cs
+++ AngularP/AngularP/Models/WeatherForecast2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -15,20 +16,26 @@ namespace AngularP.Models
         [JsonProperty]
         public string TimeZone { get; set; }
         [JsonProperty]
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
         [JsonProperty]
         public string Cod { get; set; }
         [JsonProperty]
+        [Required]
         public Main Main { get; set; }
+        [JsonProperty("weather")]
+        public ICollection<Weather> WeatherCollection { get; set; }
         [JsonProperty]
-        public ICollection<Weather> Weather { get; set; }
-        [JsonProperty]
+        [Required]
         public int Visibility { get; set; }
         [JsonProperty]
+        [Required]
         public Wind Wind { get; set; }
         [JsonProperty]
         public Clouds Clouds { get; set; }
         [JsonProperty("dt")]
+        [Required]
         public string DateTime { get; set; }
         [JsonProperty]
         public Sys Sys { get; set; }
